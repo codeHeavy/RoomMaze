7685a6d23ab3993b45b9c804291c16f3904aceba
Jishnu Girish <jishnugirish@gmail.com>
2017-12-05 22:36:23 -0500

    Added first person view and minimap

---
+++ Assets/Scripts/CameraOverlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOverlay : MonoBehaviour {
+    [Range(0.2f,1f)]
+    public float overlayWidth = 0.5f;
+    [Range(0.2f, 1f)]
+    public float overlayHeight = 0.5f;
+    [Range(-100,100)]
+    public float xPos = 0;
+    [Range(-100, 100)]
+    public float yPos = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Camera.main.rect = new Rect(xPos, yPos, overlayWidth, overlayHeight);
+        Camera.main.clearFlags = CameraClearFlags.Depth;
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,21 +13,21 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey("up"))//Press up arrow key to move forward on the Y AXIS
+        if (Input.GetKey("w"))//Press up arrow key to move forward on the Y AXIS
         {
             transform.position += transform.forward * playerSpeed * Time.fixedDeltaTime;
         }
-        if (Input.GetKey("down"))
+        if (Input.GetKey("s"))
         {
             transform.position += -transform.forward * playerSpeed * Time.fixedDeltaTime;
         }
-        if (Input.GetKey("right")) //Right arrow key to turn right
+        if (Input.GetKey("d")) //Right arrow key to turn right
         {
             transform.position += transform.right * playerSpeed * Time.fixedDeltaTime;
             //StartCoroutine("TurnLeft");
         }
 
-        if (Input.GetKey("left"))//Left arrow key to turn left
+        if (Input.GetKey("a"))//Left arrow key to turn left
         {
             transform.position += -transform.right * playerSpeed * Time.fixedDeltaTime;
             //StartCoroutine("TurnRight");
@@ -55,10 +55,14 @@ public class PlayerMovement : MonoBehaviour {
 
     public void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "enemy")
+        if (other.gameObject.tag == "enemy")
         {
             //Debug.Log("Attacking...");
             other.gameObject.GetComponent<EnemyProperties>().TakeDamage(attackDamage);
+        }
+        if (other.gameObject.tag == "Wall")
+        {
+            Debug.Log("Hit wall");
         }
     }
 }
